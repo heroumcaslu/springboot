d657c6c2779cb1a7b1058cbddd172725e5f6b76f
Lucas <lucas_herom@hotmail.com>
2020-11-29 00:11:54 -0300

    service layer

--- src/main/java/com/heroumcaslu/springboot/services/StudentService.java
+++ src/main/java/com/heroumcaslu/springboot/services/StudentService.java
@@ -1,11 +1,21 @@
 package com.heroumcaslu.springboot.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.heroumcaslu.springboot.error.ResourceNotFoundException;
 import com.heroumcaslu.springboot.repositories.StudentRepository;
 
+@Service
 public class StudentService {
 	
 	private static StudentRepository studentDAO;
+	
+	@Autowired
+	public StudentService(StudentRepository studentDAO) {
+		// TODO Auto-generated constructor stub
+		StudentService.studentDAO = studentDAO;
+	}
 	
 	public static void verifyIfStudentExists(Long id) {
 		
